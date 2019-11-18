3a7ff2193767df87c1d6a1d2c2d1bae002358634
raginpirate <danielcwyckoff@gmail.com>
2019-11-18 11:13:05 -0500

    copied over @worst PR for throttle exceeded behavior

--- lib/sidekiq/throttler.rb
+++ lib/sidekiq/throttler.rb
@@ -36,18 +36,38 @@ module Sidekiq
         yield
       end
 
-      rate_limit.exceeded do |delay|
-        # Sidekiq is going to run it at the given time
-        # partially copied from Sidekiq::Worker#perform_in https://github.com/mperham/sidekiq/blob/4eb54965dc0acf1920d2d0eb8c678b1f77efd0c9/lib/sidekiq/worker.rb#L55
-        interval = delay.to_f
-        now = Time.now.to_f
-        ts = (interval < 1_000_000_000 ? now + interval : interval)
-        msg['at'] = ts unless ts <= now
+      # We now allow for explicitly setting the exceeded behavior.
+      # By default (or with a :retry value), the exceeded behavior follows the
+      # previous behavior and retries the job.
+      #
+      # Otherwise, it attempts to use the behavior specified in the class options
+      # The specified behavior should be a a proc that takes up to 4 arguments:
+      # 1: the period of the rate limiter
+      # 2: the worker for the job
+      # 3: the message payload
+      # 4: the queue the job was pulled from
+      # NB: The exceeded behavior passed *MUST* be a proc if you are using less
+      # then 4 arguments. Because the rate limiter itself always passes four
+      # arguments it doesn't work with a lambda.
 
-        # Using .client_push instead of .perform_in to send all the data we have about the worker (like queue, retry, throttle), not only the args
-        # By doing so the worker doesn't need to ask for the sidekiq options again by calling Sidekiq::Client.get_sidekiq_options
-        # Before it was: worker.class.perform_in(delay, *msg['args'])
-        worker.class.client_push(msg)
+      worker_options = (worker.class.get_sidekiq_options['throttle'] || {}).stringify_keys
+
+      if worker_options['exceeded'].nil? || worker_options['exceeded'] == :retry
+        rate_limit.exceeded do |delay|
+          # Sidekiq is going to run it at the given time
+          # partially copied from Sidekiq::Worker#perform_in https://github.com/mperham/sidekiq/blob/4eb54965dc0acf1920d2d0eb8c678b1f77efd0c9/lib/sidekiq/worker.rb#L55
+          interval = delay.to_f
+          now = Time.now.to_f
+          ts = (interval < 1_000_000_000 ? now + interval : interval)
+          msg['at'] = ts unless ts <= now
+
+          # Using .client_push instead of .perform_in to send all the data we have about the worker (like queue, retry, throttle), not only the args
+          # By doing so the worker doesn't need to ask for the sidekiq options again by calling Sidekiq::Client.get_sidekiq_options
+          # Before it was: worker.class.perform_in(delay, *msg['args'])
+          worker.class.client_push(msg)
+        end
+      else
+        rate_limit.exceeded &worker_options['exceeded']
       end
 
       rate_limit.execute
